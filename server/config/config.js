140b958caf030afe2cd4f2b9a975ba201233c2e4
Gabriel <garciagd7@gmail.com>
2018-09-10 17:13:48 -0500

    RestServe - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,21 @@
 //  Puerto
 // =========================================
 process.env.PORT = process.env.PORT || 3000;
+
+// =========================================
+//  Entorno
+// =========================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =========================================
+//  Base de datos
+// =========================================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:America1995@ds251632.mlab.com:51632/cafe-dev';
+}
+
+process.env.URLDB = urlDB;
