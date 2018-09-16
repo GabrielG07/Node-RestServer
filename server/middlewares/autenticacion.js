2dbf06eb06fc3ee05bf9b10478a2a29c33a126d3
Gabriel <garciagd7@gmail.com>
2018-09-15 20:11:54 -0500

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -24,8 +24,34 @@ let verificaToken = (req, res, next) => {
 
         next();
 
-    })
+    });
 };
+
+// =========================================
+//  Verificar Token para imagen
+// =========================================
+let verificaTokenImg = (req, res, next) => {
+
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no valido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario;
+
+        next();
+
+    });
+
+}
 
 // =========================================
 //  Verificar AdminRole
@@ -51,5 +77,6 @@ let verificaAdmin_Role = (req, res, next) => {
 
 module.exports = {
     verificaToken,
-    verificaAdmin_Role
+    verificaAdmin_Role,
+    verificaTokenImg
 };
